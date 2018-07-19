dfdb50807ade003c5b3ceb90971ab215be239ddd
Fein <Noam.Fein@aristocrat.com>
2018-07-19 10:41:39 -0500

    add string extensions

---
+++ NFSystems.Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NFSystems.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmpty(this string value) => string.IsNullOrEmpty(value);
+
+        public static bool IsNotEmpty(this string value) => !string.IsNullOrEmpty(value);
+
+        public static string[] SplitCamelCase(this string source) => Regex.Split(source, @"(?<!^)(?=[A-Z])");
+    }
+}
